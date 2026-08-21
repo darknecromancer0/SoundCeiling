7faed4c1d4e355e04da61b8e0df48b55556558b7
darknecromancer0 <127566984+darknecromancer0@users.noreply.github.com>
2026-08-21 22:31:33 +0300

    test: require manual threshold follower runtime integration

--- scripts/check-v06-one-way-contract.sh
+++ scripts/check-v06-one-way-contract.sh
@@ -23,6 +23,18 @@ require "$PKG/NormalizerService.java" 'APP_WRITE_ACK'
 require "$PKG/NormalizerService.java" 'APP_WRITE_MISMATCH'
 require "$PKG/NormalizerService.java" 'external_zero_detected'
 
+# v0.6 manual intent is represented as a dB threshold offset, never a recovering Media envelope.
+require "$PKG/ManualThresholdFollower.java" 'DECREASE_TAU_MS = 120L'
+require "$PKG/ManualThresholdFollower.java" 'RESTORE_TAU_MS = 650L'
+require "$PKG/NormalizerService.java" 'ManualThresholdFollower manualThreshold'
+require "$PKG/NormalizerService.java" 'manualThreshold.onUserChange'
+require "$PKG/NormalizerService.java" 'manualThreshold.onDeliberateLowering'
+require "$PKG/NormalizerService.java" 'manualThreshold.effectiveThreshold(p.targetLoudness)'
+require "$PKG/NormalizerService.java" 'manualThreshold.effectiveThreshold(p.sourcePeakThresholdDbfs)'
+require "$PKG/NormalizerService.java" 'ordinaryNormalizationPaused(current, controlCurve.minIndex())'
+reject "$PKG/NormalizerService.java" 'manualSafety.effectiveMax()'
+reject "$PKG/NormalizerService.java" 'manualSafety.isManualSafetyPause()'
+
 require "$PKG/LoudnessControlPolicy.java" 'below_target_hold'
 require "$PKG/QuietNowPolicy.java" 'return Math.min(current, quiet);'
 
