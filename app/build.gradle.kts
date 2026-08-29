d4f3e5c99ce771e6fa3ac3502624ab4e5f6a45d4
darknecromancer0 <127566984+darknecromancer0@users.noreply.github.com>
2026-08-29 21:09:56 +0300

    chore(v0.7.7.9): bump Android version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins { id("com.android.application") }
 android { namespace="dev.soundceiling.app"; compileSdk=35
- defaultConfig { applicationId="dev.soundceiling.app"; minSdk=29; targetSdk=35; versionCode=32; versionName="0.7.7.8" }
+ defaultConfig { applicationId="dev.soundceiling.app"; minSdk=29; targetSdk=35; versionCode=33; versionName="0.7.7.9" }
  buildFeatures { buildConfig = true }
  signingConfigs {
   create("soundCeilingDev") {
