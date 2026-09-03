8c117bae024718dfabaa2b56e5ce7f15f3944c6c
darknecromancer0 <127566984+darknecromancer0@users.noreply.github.com>
2026-09-03 14:59:45 +0300

    v0.9.2: bump field build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins { id("com.android.application") }
 android { namespace="dev.soundceiling.app"; compileSdk=35
- defaultConfig { applicationId="dev.soundceiling.app"; minSdk=29; targetSdk=35; versionCode=37; versionName="0.9.1" }
+ defaultConfig { applicationId="dev.soundceiling.app"; minSdk=29; targetSdk=35; versionCode=38; versionName="0.9.2" }
  buildFeatures { buildConfig = true }
  signingConfigs {
   create("soundCeilingDev") {
